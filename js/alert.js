4a56fcc2a65ee1d0bf3b6aa5e12fc35f8273be63
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro-2.local>
2019-06-04 10:50:47 -0500

    add files

---
+++ js/alert.js
@@ -0,0 +1,10 @@
+// ALERT // 
+
+var paragraph= "Written";
+	if ("Written") {
+		window.alert ("Be sure to read the important information on Jaguars below");
+	}
+	else {
+		window.alert ("Be sure to stay up to date with information on Jaguar endagerment");
+	}
+
